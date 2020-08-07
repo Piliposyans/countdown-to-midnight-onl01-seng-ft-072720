8f8b82a0cdaf4036b87854c2ffe95c3e82b8ba76
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-07 23:03:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 count = 10 
 while count > 0 
-puts "#{count} SECOND(S)!"
-count -= 1
-break if count = 0 
+  puts "#{count} SECOND(S)!"
+  count -= 1
+  break if count = 0 
 end
 
 puts "HAPPY NEW YEAR!"
